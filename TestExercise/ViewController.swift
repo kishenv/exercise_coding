29fd798e8393a3d476eaff01d79ec3ffe0e72e2f
Kishen Vinod <kv0714@c02qh11xfvh6.satx.rr.com>
2017-04-15 23:48:14 -0500

    Initial Commit

---
+++ TestExercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestExercise
+//
+//  Created by Kishen Vinod on 4/15/17.
+//  Copyright © 2017 Kishen Vinod. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
